886df1f0724d9426073fa2c44f6dac8c2cde87e2
Gergely Nagy <152151245+ngergely0@users.noreply.github.com>
2024-02-26 12:10:27 +0100

    na most Final touches

--- CountiesDbTools.php
+++ CountiesDbTools.php
@@ -52,8 +52,6 @@ class CountiesDbTools {
  
         public function getAllFlags()
     {
-        //$allFlags = ["pics/Bacs-Kiskun.png","pics/Baranya.png","pics/Bekes.png","pics/Borsod-Abauj-Zemplen.png","pics/Csongrad-Csanad.png","pics/Fejer.png","pics/Gyor-Moson-Sopron.png","pics/Hajdu-Bihar.png","pics/Heves.png","pics/Jasz-Nagykun-Szolnok.png","pics/Komarom-Esztergom.png","pics/Nograd.png","pics/Pest.png","pics/Somogy.png","pics/Szabolcs-Szatmar.png","pics/Tolna.png","pics/Vas.png","pics/Veszprem.png","pics/Zala.png"];
-        
         $sql = "SELECT flag FROM counties";
         $result = $this->mysqli->query($sql);
  
@@ -62,6 +60,11 @@ class CountiesDbTools {
                 $allFlags[] = $row;
             }
         }
+        $newString = "";
+
+
+        array_unshift($allFlags, $newString);
+        
         return $allFlags;
     }
     
--- city.php
+++ city.php
@@ -1,82 +1,25 @@
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Városok</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="fontawesome/css/all.css" type="text/css">
-</head>
-<body>
- 
-<h1>Városok</h1>
- 
-<?php
-        echo '
-            <form method="post" action="cities.php">
-            <button id ="Home" name="Home"><a href="cities.php"><i class="fa fa-home" title="Kezdőlap"></i></a></button>
-            </form>
-            <form method="post" action="export.php">
-                <button id="btn-export" name="btn-export" title="Export to .CSV">
-                    <i class="fa fa-file-excel"></i>&nbsp;Export CSV</button>
-            </form>';
-        
- ?>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" onsubmit="setCountyId()">
- 
-    <label for="countyDropdown">Megye:</label>
-    <select id="countyDropdown" name="countyDropdown">
-        <option value="">Válassz megyét</option>
-        <?php
-        require_once('CountiesDbTools.php');
-        $countiesDbTool = new CountiesDbTools();
-        require_once('CitiesDbTools.php');
-        $citiesDbTool = new CitiesDbTools();
-        $selectedCountyId = null;
-        $counties = $countiesDbTool->getAllCounties();
-        foreach ($counties as $county) {
-            echo '<option value="' . $county['id'] . '">' . $county['name'] . '</option>';
-        }
-        ?>
-    </select>
-    <input type="submit" name="submit" value="Küldés">
-        <form method="post" action="cities.php">
-            <input type="text" name="needle" value="">
-            <button class="search" type="submit" name="btn-search" method="post">Keres</button>
-        </form>
-        <br>
-    <input type="hidden" name="county_id" id="county_id" value="">
-</form>
- 
- 
- 
- 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+<?php
    
-    <?php
-    if (isset($_POST["countyDropdown"])) {
+    require_once('AbstractPage.php');
+    require_once('CountiesDbTools.php');
+    require_once('CitiesDbTools.php');
+
+    $countiesDbTool = new CountiesDbTools();
+    $citiesDbTool = new CitiesDbTools();
+
+    AbstractPage::insertHtmlHead();
+    AbstractPage::showExportBtn();
+    $counties = $countiesDbTool->getAllCounties();
+    AbstractPage::showDropDown($counties);
+    AbstractPage::showAddCity();
+
+    if (isset($_POST["countyDropdown"])) 
+    {
         $selectedCountyId = isset($_POST["countyDropdown"]) ? $_POST["countyDropdown"] : '';
-    }
-    ?>
-    <input type="hidden" name="county_id" value="<?php echo isset($selectedCountyId) ? $selectedCountyId : ''; ?>">
-    <label for="new_city_name">Új város neve:</label>
-    <input type="text" id="new_city_name" name="new_city_name">
-    <label for="new_city_zip">Irányítószám:</label>
-    <input type="text" id="new_city_zip" name="new_city_zip">
-    <input type="hidden" name="id_county" value="<?php echo $selectedCountyId; ?>">
-    <input type="submit" name="add_city" value="Hozzáad">
-</form>
- 
-    <?php
-    if (isset($_POST["countyDropdown"])) {
-       
- 
-        $selectedCountyId = isset($_POST["countyDropdown"]) ? $_POST["countyDropdown"] : '';
-       
         $cities = $citiesDbTool->getCitiesByCountyId($selectedCountyId);
-        $population = [503825,360704,334264,642447,399012,417712,467144,527989,294609,370007,299207,189304,1278874,301429,552964,217463,253551,341317,268648];
-        $CountySeat = ["Kecskemét", "Pécs", "Békéscsaba", "Miskolc", "Szeged", "Székesfehérvár", "Győr", "Debrecen", "Eger", "Szolnok", "Tatbánya", "Salgótarján", "Budapest", "Kaposvár", "Nyíregyháza", "Szekszárd", "Szombathely", "Veszprém", "Zalaegerszeg"];
+        $population = ["",503825,360704,334264,642447,399012,417712,467144,527989,294609,370007,299207,189304,1278874,301429,552964,217463,253551,341317,268648];
+        $CountySeat = ["","Kecskemét", "Pécs", "Békéscsaba", "Miskolc", "Szeged", "Székesfehérvár", "Győr", "Debrecen", "Eger", "Szolnok", "Tatbánya", "Salgótarján", "Budapest", "Kaposvár", "Nyíregyháza", "Szekszárd", "Szombathely", "Veszprém", "Zalaegerszeg"];
         $allFlags = $countiesDbTool->getAllFlags();
        
         if (isset($_POST['btn-search'])) {
@@ -91,36 +34,8 @@
             $idxPopulation = $cities[0]['id_county'];
             $idxCountySeat = $cities[0]['id_county'];
             $idxFlag = $cities[0]['id_county'];
-            echo '<div class="container">';
-            echo '            <table class = "data" >';
-            echo '    <tr>';
-            echo '        <td colspan="3" class="border"><img class="flag-img" src="' . $allFlags[$idxFlag-1]['flag'] . '" alt="County Flag"></td>';
-            echo '    </tr>';
-            echo '    <tr>';
-            echo '        <td>Népesség:</td>';
-            echo '        <td>' . $population[$idxPopulation-1] . '</td>';
-            echo '    </tr>';
-            echo '    <tr>';
-            echo '        <td>Megyeszékhely:</td>';
-            echo '        <td>' . $CountySeat[$idxCountySeat-1] . '</td>';
-            echo '    </tr>';
-            echo '</table>';
-            echo '</div>';
-            
-
-            //echo 'Népesség:' . $population[$idxPopulation-1] . '    Megyeszékhely:' . $CountySeat[$idxCountySeat-1] . '   Zászló:' . '<img class = "flag-img" src="' . $allFlags[$idxFlag-1] . '" alt="County Flag">';
-            echo '<table>';
-            echo '<tr><th>Irányítószám</th><th>Város</th><th>Megye</th><th class="muveletek" colspan="2">Műveletek</th></tr>';
-            foreach ($cities as $city) {
-                echo '<tr>';
-                echo '<td>' . $city['zip_code'] . '</td>';
-                echo '<td>' . $city['city'] . '</td>';
-                echo '<td>' . $city['county_name'] . '</td>';
-                echo '<td><form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '"><input type="hidden" name="city_id" value="' . $city['id'] . '"><input type="submit" name="delete_city" value="Törlés"></form></td>';
-                echo '<td><form method="post" action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '"><input type="hidden" name="modify_city_id" value="' . $city['id'] . '"><input type="submit" name="modify_city" value="Módosítás"></form></td>';
-                echo '</tr>';
-            }
-            echo '</table>';
+            AbstractPage::showContainer($allFlags, $idxFlag, $population, $idxPopulation, $CountySeat, $idxCountySeat);
+            AbstractPage::showMainTable($cities);
         }
     }
  
@@ -133,17 +48,8 @@
     if (isset($_POST['modify_city'])) {
        
         $modifyCityId = $_POST['modify_city_id'];
-        $cityToModify = $citiesDbTool->getCityById($modifyCityId); 
-        ?>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-            <input type="hidden" name="modify_city_id" value="<?php echo $modifyCityId; ?>">
-            <label for="modified_city_name">Módosított város neve:</label>
-            <input type="text" id="modified_city_name" name="modified_city_name" value="<?php echo $cityToModify['city']; ?>">
-            <label for="modified_city_zip">Módosított irányítószám:</label>
-            <input type="text" id="modified_city_zip" name="modified_city_zip" value="<?php echo $cityToModify['zip_code']; ?>">
-            <input type="submit" name="modify_city_submit" value="Mentés">
-        </form>
-        <?php
+        $cityToModify = $citiesDbTool->getCityById($modifyCityId);
+        AbstractPage::showModifyCity($cityToModify, $modifyCityId);
     }
 
     if (isset($_POST['modify_city_submit'])) {
@@ -168,9 +74,5 @@
             echo "Kérlek töltsd ki mindkét mezőt!";
         }
     }
-   
+
     ?>
-   
-</form>
-</body>
-</html>
